e1262d7646b7819b4a477c68c69543ddf4048e64
lxvs <jn.apsd@gmail.com>
2022-01-01 18:43:56 +0800

    Add some features
     - dry run
    
     - use '-' instead of '/' as switch prefix to be more portable
    
     - add help
    
     - replace extension option with file paterrn parameter
    
     - break when no operation

--- tvren.bat
+++ tvren.bat
@@ -1,20 +1,27 @@
 @echo off
 @setlocal EnableDelayedExpansion
 chcp 65001 >nul
+set dryrun=
 set pfx=
 set sfx=
-set ext=
 set fl=
 set sf=
 set st=
 set xf=
 set xt=
+set pattern=
 :parse_args
 if "%~1" == "" goto end_parse_args
+if "%~1" == "/?" goto help
 set "sw=%~1"
 shift
-if "%sw:~0,1%" NEQ "/" (
-    echo haha
+if "%sw:~0,1%" NEQ "-" (
+    if not defined pattern (
+        set "pattern=%sw%"
+    ) else (
+        >&2 echo error: invalid argument: %sw%
+        exit /b 1
+    )
     goto parse_args
 )
 set "sw=%sw:~1%"
@@ -22,6 +29,10 @@ if "%sw%" == "?" goto help
 if /i "%sw%" == "h" goto help
 if /i "%sw%" == "help" goto help
 if /i "%sw%" == "usage" goto help
+if /i "%sw%" == "n" (
+    set dryrun=1
+    goto parse_args
+)
 if /i "%sw%" == "p" (
     set "pfx=%~1"
     shift
@@ -39,11 +50,6 @@ if /i "%sw%" == "s" (
 )
 if /i "%sw%" == "suffix" (
     set "sfx=%~1"
-    shift
-    goto parse_args
-)
-if /i "%sw%" == "ext" (
-    set "ext=%~1"
     shift
     goto parse_args
 )
@@ -68,14 +74,14 @@ if /i "%sw%" == "xt" (
     goto parse_args
 )
 >&2 echo error: invalid switch "%sw%"
->&2 call:help
 exit /b 1
 :end_parse_args
 
-if not defined ext goto end_ext
-if "%ext:~0,1%" NEQ "." set "ext=.%ext%"
-set "ext=*%ext%"
-:end_ext
+if not defined pfx if not defined sfx if not defined sf if not defined xt (
+    >&2 echo no operation
+    exit /b
+)
+
 if not defined xf goto end_xf
 if "%xf:~0,1%" NEQ "." set "xf=.%xf%"
 :end_xf
@@ -83,7 +89,7 @@ if not defined xt goto end_xt
 if "%xt:~0,1%" NEQ "." set "xt=.%xt%"
 :end_xt
 
-for /f "tokens=*" %%i in ('dir /b /a-d /on %ext%') do (
+for /f "tokens=*" %%i in ('dir /b /a-d-h-s /on %pattern%') do (
     set "fn=%%~ni"
     set "ex=%%~xi"
     if "!fn!" NEQ "" if "!ex!" NEQ "" (
@@ -103,9 +109,19 @@ for /f "tokens=*" %%i in ('dir /b /a-d /on %ext%') do (
         )
         if defined pfx set "fn=%pfx%!fn!"
         if defined sfx set "fn=!fn!%sfx%"
-        ren "%%~i" "!fn!!ex!"
+        if "%%~i" NEQ "!fn!!ex!" if defined dryrun (
+             @echo %%~i  -^>  !fn!!ex!
+        ) else ren "%%~i" "!fn!!ex!"
     )
 )
 exit /b
 
 :help
+@echo usage: tvren [-p {prefix}] [-s {suffix}] [-sf {sfrom}] [-st {sto}] [ [-xf {xfrom}] -xt {xto}]
+@echo;
+@echo  - Subtitute {sfrom} with {sto} in filenames ^(not including file extension^) in current folder
+@echo  - If both {xfrom} and {xto} are specified, change extensions of files that with extension {xfrom} in current folder
+@echo    to {xto}
+@echo  - If {xto} is specified without {xfrom}, change extensions of all files that has an extension in current folder to
+@echo    {xto}
+exit /b
